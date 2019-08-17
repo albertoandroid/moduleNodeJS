cc89fb7b595956ff2e0b23f6148c28bc358a5476
Alberto <info@androiddesdecero.com>
2019-08-17 20:23:29 +0200

    fin

--- http.js
+++ http.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+/*
 const server = http.createServer();
 
 server.on('connection', (socket)=>{
@@ -8,3 +9,30 @@ server.on('connection', (socket)=>{
 
 server.listen(2012)
 console.log('Escuchando en el puerto 2012...');
+*/
+
+/*
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.write('hola mundo');
+        res.write('desde NodeJS');
+        res.end();
+    }
+
+    if(req.url == '/coches'){
+        res.write('coche1');
+        res.end();
+    }
+});
+server.listen(3030);
+console.log('Escuchando en puerto 3030...');
+*/
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1> Hola a todos</h1>');
+    res.write('<p> Mi web de coches</p>');
+    res.end();
+}).listen(5050);
+
+
