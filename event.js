992f33007d38205a7fd5a0da475a8a3893117ab1
Alberto <info@androiddesdecero.com>
2019-08-17 09:41:44 +0200

    3

--- event.js
+++ event.js
@@ -8,3 +8,14 @@ emitter.on('event', function(){
 
 emitter.emit('event');
 
+emitter.on('eventWithArgument', function(arg){
+    console.log('Un evento con los siguientes argumentos ha ocurrido: ' + arg.id + ' ' + arg.numero);
+})
+
+emitter.emit('eventWithArgument', {id: 1, numero: 24});
+
+emitter.on('eventArrow', (arg)=>{
+    console.log('Un evento FLECHA con los siguientes argumentos ha ocurrido: ' + arg.id + ' ' + arg.numero);
+})
+
+emitter.emit('eventArrow', {id: 1, numero: 24});
